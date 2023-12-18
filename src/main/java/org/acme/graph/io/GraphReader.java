63309869513e7a08fb0d89740949cfa01459187a
outmrhoust <hicham.outmrhoust@ensg.eu>
2023-12-18 13:59:35 +0100

    0.3 exercice (part2)

--- src/main/java/org/acme/graph/io/GraphReader.java
+++ src/main/java/org/acme/graph/io/GraphReader.java
@@ -97,7 +97,7 @@ public class GraphReader {
 		if (sens.equals(DOUBLE_SENS) || sens.equals(SENS_INVERSE)) {
 			/* Création de l'arc pour le parcours en sens opposé */
 			Edge reverseEdge = graph.createEdge(target, source, id + "-reverse");
-			reverseEdge.setGeometry(geometry);
+			reverseEdge.setGeometry(geometry.reverse());
 		}
 	}
 
